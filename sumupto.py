5313646eb1523b9dd9ce24da5adfc3fdb208d67d
Henk Tjalsma <G00376321@gmit.ie>
2019-02-17 22:35:32 +0000

    Including while loop

--- sumupto.py
+++ sumupto.py
@@ -1,10 +1,15 @@
 # Solution to problem 1
 # Write a program that asks the user to input any positive integer and outputs the sum of all numbers between one and that number. 
 # The number is recognized as a text string, and has to be converted to integer. Input from user has to be converted to integer.
-# Consider negative numbers, and strings in the code.
-x = int(input("Please enter an integer: "))
+# Consider negative numbers, and strings in the code. How? Possible?
+# Including while loop.
+i = int(input("Please enter a positive integer: "))
 
-if x >= 0:
-    print('That is posive integer')
-else:
-    print('That is not a posive integer. Try again!')
+total = 0
+
+while i > 0:
+# The new total value is on left hand side, old total on the right hand side.    
+    total = total + i
+    i = i -1
+
+print(total)    
