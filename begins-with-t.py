016dee9762022856f4efe6b1d001458adf732790
Henk Tjalsma <G00376321@gmit.ie>
2019-02-23 19:54:40 +0000

    Some experimenting with datetime module in begins-with-t.py script

--- begins-with-t.py
+++ begins-with-t.py
@@ -1,13 +1,38 @@
 # Henk Tjalsma, 2019
 # Write a program that outputs whether or not today is a day that begins with the letter T.
 # Verbatim: https://stackoverflow.com/a/9847269 - Return the day of the week as an integer, where Monday is 0 and Sunday is 6.
-# Tuesday equals 1, and Thursday 3.
+# https://www.pythonprogramming.in/date-time/get-the-day-of-week-from-given-a-date-in-python.html
 
+# import Python's datetime module
 import datetime
 
-if datetime.datetime.today().weekday == 1:
-    print("Yes - today begins with a T.")   
-elif datetime.datetime.today().weekday == 3:  
-    print("Yes - today begins with a T.") 
+# print("Present year: ", datetime.date.today().strftime("%Y"))
+# print("Month of year: ", datetime.date.today().strftime("%B"))
+# print("Week number of the year: ", datetime.date.today().strftime("%W"))
+# print("Weekday of the week: ", datetime.date.today().strftime("%w"))
+# print("Day of year: ", datetime.date.today().strftime("%j"))
+# print("Day of the month : ", datetime.date.today().strftime("%d"))
+# print("Day of week: ", datetime.date.today().strftime("%A"))
+
+# Testing on Saturday 23/02/2019
+# if datetime.datetime.today().weekday() == 5:
+#  print("Yippee!")
+
+# dayofweek = datetime.date(2019, 2, 23).strftime("%A")
+
+# Tuesday - begins with T, weekday 1
+if datetime.datetime.today().weekday() == 1:
+   print("Yes - today begins with a T.")
+#  print(dayofweek)
+   print("weekday():", datetime.datetime.today().weekday())   
+   print("isoweekday()", datetime.datetime.today().isoweekday())
+# Thursday - begins with T, weekday 3   
+elif datetime.datetime.today().weekday() == 3:  
+  print("Yes - today begins with a T.")
+#  print(dayofweek) 
+  print("weekday():", datetime.datetime.today().weekday())
+  print("isoweekday()", datetime.datetime.today().isoweekday())
 else:
-    print("No - today does not begin with a T.")    
+   print("No - today does not begin with a T.")    
+
+
