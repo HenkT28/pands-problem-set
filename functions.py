7b01e61b0e6b2a67fbc4a24a323288a928b6d262
Henk Tjalsma <G00376321@gmit.ie>
2019-03-02 22:17:52 +0000

    Modifying solution 10, done 020319:22:17

--- functions.py
+++ functions.py
@@ -2,6 +2,12 @@
 # Solution to problem 10 - functions.py script
 # Write a program that displays a plot of the functions x, x2 and 2x in the range [0, 4].
 # https://realpython.com/python-matplotlib-guide/
+# https://matplotlib.org/1.4.2/users/pyplot_tutorial.html
 
-import matplotlib
-import numpy
+#import matplotlib
+import matplotlib.pyplot as plt
+# import numpy
+
+plt.plot([0,4])
+plt.ylabel('some numbers')
+plt.show()
