ddc7a72de5bd03a4125c82236341578b3d8405e2
Henk Tjalsma <G00376321@gmit.ie>
2019-03-02 22:10:31 +0000

    Cleaning up pands directory, _pycache_ folder existing, and added template for solution 10, done 020319:22:09

---
+++ functions.py
@@ -0,0 +1,7 @@
+# Henk Tjalsma, 2019
+# Solution to problem 10 - functions.py script
+# Write a program that displays a plot of the functions x, x2 and 2x in the range [0, 4].
+# https://realpython.com/python-matplotlib-guide/
+
+import matplotlib
+import numpy
