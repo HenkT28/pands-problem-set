d96fda111c6f92d1fcc641387e886e7b27f85e8d
Henk Tjalsma <G00376321@gmit.ie>
2019-03-17 23:12:39 +0000

    Simplified begins-with-t.py script, included or statement for both days, both conditions, 170319:23:11

--- begins-with-t.py
+++ begins-with-t.py
@@ -7,33 +7,19 @@
 # import Python's datetime module
 import datetime
 
-# print("Present year: ", datetime.date.today().strftime("%Y"))
-# print("Month of year: ", datetime.date.today().strftime("%B"))
-# print("Week number of the year: ", datetime.date.today().strftime("%W"))
-# print("Weekday of the week: ", datetime.date.today().strftime("%w"))
-# print("Day of year: ", datetime.date.today().strftime("%j"))
-# print("Day of the month : ", datetime.date.today().strftime("%d"))
-# print("Day of week: ", datetime.date.today().strftime("%A"))
-
-# Testing on Saturday 23/02/2019
-# if datetime.datetime.today().weekday() == 5:
+# Testing on Sunday 17/03/2019
+# if datetime.datetime.today().weekday() == 6:
 #  print("Yippee!")
 
-# dayofweek = datetime.date(2019, 2, 23).strftime("%A")
-
-# Tuesday - begins with T, weekday 1
-if datetime.datetime.today().weekday() == 1:
+# Tuesday and Thursday - both begin with T, weekday 1, and weekday 3
+# Using single or statement
+if datetime.datetime.today().weekday() == 1 or datetime.datetime.today().weekday() == 3:
    print("Yes - today begins with a T.")
-#  print(dayofweek)
    print("weekday():", datetime.datetime.today().weekday())   
    print("isoweekday()", datetime.datetime.today().isoweekday())
-# Thursday - begins with T, weekday 3   
-elif datetime.datetime.today().weekday() == 3:  
-  print("Yes - today begins with a T.")
-#  print(dayofweek) 
-  print("weekday():", datetime.datetime.today().weekday())
-  print("isoweekday()", datetime.datetime.today().isoweekday())
 else:
-   print("No - today does not begin with a T.")    
+   print("No - today does not begin with a T.")
+   print("weekday():", datetime.datetime.today().weekday())   
+   print("isoweekday()", datetime.datetime.today().isoweekday())    
 
 
