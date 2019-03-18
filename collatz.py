d26fa38d60de240285fa74d86a41fb6fdcf4d21f
Henk Tjalsma <G00376321@gmit.ie>
2019-03-18 02:23:21 +0000

    Added some code to collatz.py script, 180319:02:22

--- collatz.py
+++ collatz.py
@@ -1,4 +1,25 @@
 # Henk Tjalsma, 2019
 # Solution to problem 4 - collatz.py
 # Write a program that asks the user to input any positive integer and outputs the successive values of the following calculation. 
-# At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one. Have the program end if the current value is one.
+# At each step calculate the next value by taking the current value:
+# ...if it is even, divide it by two
+# ...but if it is odd, multiply it by three and add one. 
+# Have the program end if the current value is one.
+
+# http://www.openbookproject.net/thinkcs/python/english2e/ch06.html
+
+n = int(input("Please enter a positive integer: "))
+
+# def next_value(n):
+# The condition for this loop is n != 1, so it will continue until n is 1, which will make the condition false.    
+
+# A number is even if division by 2 give a remainder of 0.
+# If remainder is 1, it is odd number.
+
+while n != 1:
+    print (n, end=' ')
+    if n % 2 == 0: 
+        n = n / 2   # n is even
+    else:  
+        n = n * 3 + 1   # n is odd
+            
