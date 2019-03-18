06e58246d0a5dcd4a1db6b330f8aea83d5557f44
Henk Tjalsma <G00376321@gmit.ie>
2019-03-18 00:11:05 +0000

    Completed secondstring.py script, 180319:00:10

--- secondstring.py
+++ secondstring.py
@@ -1,3 +1,16 @@
 # Henk Tjalsma, 2019
 # Solution to problem 6 - secondstring.py script
 # Write a program that takes a user input string and outputs every second word.
+# https://stackoverflow.com/questions/54857129/write-a-program-that-takes-a-user-input-string-and-outputs-every-second-word
+# https://www.pythoncentral.io/cutting-and-slicing-strings-in-python/
+# https://stackoverflow.com/questions/509211/understanding-slice-notation
+
+# Enter the user input string
+sentenceInput=(input("Please enter sentence: "))
+
+# string = 'The quick brown fox jumps over the lazy dog.'
+# every_second_word = sentenceInput.split(' ')[::2]
+
+# You split the original string using spaces, then you take every other word from it with the [::2] splice.
+every_second_word = sentenceInput.split(' ')
+print(every_second_word[::2])
