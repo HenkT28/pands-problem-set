38adac706839af7fad271b8afce56914a57a4b35
Henk Tjalsma <G00376321@gmit.ie>
2019-03-18 00:51:53 +0000

    Finished functions.py script, 180319:00:51

--- functions.py
+++ functions.py
@@ -3,11 +3,27 @@
 # Write a program that displays a plot of the functions x, x2 and 2x in the range [0, 4].
 # https://realpython.com/python-matplotlib-guide/
 # https://matplotlib.org/1.4.2/users/pyplot_tutorial.html
+# https://stackoverflow.com/questions/46513843/plot-math-function-fx-with-varying-values-of-x-and-for-different-values-of-n
+# https://techmonger.github.io/2/simple-parabola-matplotlib/
+# https://stackoverflow.com/questions/22276066/how-to-plot-multiple-functions-on-the-same-figure-in-matplotlib
 
+# import numpy
+from numpy import *
+# import math
+import math
 #import matplotlib
 import matplotlib.pyplot as plt
-# import numpy
 
-plt.plot([0,4])
-plt.ylabel('some numbers')
+x_cords = range(0,4)
+y_x = [x for x in x_cords]
+y_2x = [2*x for x in x_cords]
+y_x2 = [x*x for x in x_cords]
+
+plt.plot(x_cords, y_x, 'r')
+plt.plot(x_cords, y_2x, 'g')
+plt.plot(x_cords, y_x2, 'b')
+
+# plt.scatter(x_cords, y_x)
+# plt.scatter(x_cords, y_2x)
+# plt.scatter(x_cords, y_x2)
 plt.show()
