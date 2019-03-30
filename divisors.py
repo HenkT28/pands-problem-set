84ed9cc8ba0c5d62f0296602e936b423b599cd01
Henk Tjalsma <G00376321@gmit.ie>
2019-03-30 16:22:50 +0000

    Changed comments in divisors.py script, 300319:16:22

--- divisors.py
+++ divisors.py
@@ -1,12 +1,15 @@
 # Henk Tjalsma, 2019
 # Solution to problem 3 - divisors.py script
 # Write a program that prints all numbers between 1,000 and 10,000 that are divisible by 6 but not 12.
+# No user input is required.
 
+# Setting range between 1,000 and 10,000 -> high end 10,001 so includes 10,000
 for n in range(1000, 10001):
 
-# Used prime test example as template, explained in the course. The division has to be without remainder: if n%6 == 0 and n%12 != 0, then print. 
-# Only if statement, no else (optional), as it needs to meet condition. 
+# Used prime test example as explained in the course, as template. 
+# The division has to be as follows: if n%6 == 0 and n%12 != 0, then print. 
+# Only used if statement here, no elif, or else (all optional), as it needs to meet the specific condition. 
 
-        if n%6 == 0 and n%12 != 0:
-            print(n)
-        
+        # Condition True
+        if n%6 == 0 and n%12 != 0:            
+            print(n)    
