3418908156f2d2b41e9337c3743b0cde0f7748d8
Henk Tjalsma <G00376321@gmit.ie>
2019-03-30 16:30:56 +0000

    Changed comments in collatz.py script, 300319:16:30

--- collatz.py
+++ collatz.py
@@ -2,26 +2,29 @@
 # Solution to problem 4 - collatz.py
 # Write a program that asks the user to input any positive integer and outputs the successive values of the following calculation. 
 # At each step calculate the next value by taking the current value:
-# ...if it is even, divide it by two
-# ...but if it is odd, multiply it by three and add one. 
+# -> If it is even, divide it by two.
+# -> But if it is odd, multiply it by three and add one. 
 # Have the program end if the current value is one.
 
+# Entering a positive integer by user.
 n = int(input("Please enter a positive integer: "))
 
 # The condition for this loop is n != 1, so it will continue until n is 1, which will make the condition false.    
 
-# A number is even if division by 2 give a remainder of 0.
-# If remainder is 1, it is odd number.
-# While statement requires else, otherwise 1 will not be included in the output.
+# A number is even if division by 2 gives a remainder of 0.
+# If remainder is 1, it is an odd number.
+# While statement requires else, otherwise 1 will not be included in the output, and break condition.
 
 # https://stackoverflow.com/questions/17651384/python-remove-division-decimal
 
 while n != 1:
     print (n, end=' ')
-    # The script kept producing a decimal while requesting a positive integer -> integer division with // that always give a int    
+    # The script will start producing a decimal during the loop while inputting a positive integer -> integer division with // that always give a int    
     if n % 2 == 0: 
-        n = n // 2   # n is even
-    else:  
-        n = n * 3 + 1   # n is odd
+        # n is even
+        n = n // 2   
+    else:
+        # n is odd  
+        n = n * 3 + 1   
 else:
     print (n, end=' ')      
